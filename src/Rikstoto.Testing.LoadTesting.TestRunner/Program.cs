364348c52c2ea958c2dedb975fe2d2450745a6fd
gabrielandreirt <85166040+gabrielandreirt@users.noreply.github.com>
2023-09-07 14:50:24 +0200

    feat(load-testing): - split setup and run

--- src/Rikstoto.Testing.LoadTesting.TestRunner/Program.cs
+++ src/Rikstoto.Testing.LoadTesting.TestRunner/Program.cs
@@ -16,76 +16,158 @@ builder.Services.AddTransient<IAccountingService, AccountingService>();
 
 using IHost host = builder.Build();
 var nrOfCores = 8.0; //check in Task Manager under the Performance tab
-var nrOfRuns = 8;
+var nrOfRunsSetup = 20;
+var nrOfRunsBets = 100;
+var fileLocation = Environment.CurrentDirectory + "\\bets.txt";
 
-for (int i = 0; i <= nrOfRuns;  i++)
+
+void SetupReservations()
 {
-    var startingCustomerId = 8_000_001;
-    var nrOfTickets = 25;
-    var count = i * nrOfTickets;
-    var numbers = Enumerable.Range(startingCustomerId + i, Math.Max(count , nrOfTickets));
-
-    Parallel.ForEach(numbers,
-                    new ParallelOptions
-                    {
-                        MaxDegreeOfParallelism = Convert.ToInt32(Math.Ceiling(Environment.ProcessorCount * 0.75 * nrOfCores))
-                    },
-                    RunFromCmdPromptAsync);
-
-    void RunFromCmdPromptAsync(int customerId)
+    for (int i = 0; i <= nrOfRunsSetup - 1; i++)
     {
-        Console.WriteLine($"customerId: {customerId}");
+        var startingCustomerId = 8_000_001;
+        var nrOfTickets = 50;
+        var count = i * nrOfTickets;
+        var numbers = Enumerable.Range(startingCustomerId + count, nrOfTickets);
 
-        var hostProvider = host.Services;
-        using IServiceScope serviceScope = hostProvider.CreateScope();
-        IServiceProvider provider = serviceScope.ServiceProvider;
-        var betLimitsServiceClient = provider.GetRequiredService<IBetLimitsService>();
-        var bettingServiceClient = provider.GetRequiredService<IBettingService>();
-        var accountServiceClient = provider.GetRequiredService<IAccountingService>();
+        Parallel.ForEach(numbers,
+                        new ParallelOptions
+                        {
+                            MaxDegreeOfParallelism = Convert.ToInt32(Math.Ceiling(Environment.ProcessorCount * 0.75 * nrOfCores))
+                        },
+                        RunFromCmdPromptAsync);
 
-        var purchaseId = new PurchaseId
+        void RunFromCmdPromptAsync(int customerId)
         {
-            Id = Guid.NewGuid().ToString()
-        };
-        var betDataList = new List<BetData>
+            Console.WriteLine($"customerId: {customerId}");
+
+            var hostProvider = host.Services;
+            using IServiceScope serviceScope = hostProvider.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var betLimitsServiceClient = provider.GetRequiredService<IBetLimitsService>();
+            var bettingServiceClient = provider.GetRequiredService<IBettingService>();
+            var accountServiceClient = provider.GetRequiredService<IAccountingService>();
+
+            var purchaseId = new PurchaseId
+            {
+                Id = Guid.NewGuid().ToString()
+            };
+            var betDataList = new List<BetData>
         {
             BetData.Parse("d:2023-01-18|t:BJ|g:V75|nt:1|w:1|org:NR|p:4900|pr:50|o:2|s1:10|s2:2|s3:46|s4:2|s5:2|s6:102|s7:22|l:1")
         };
-        var totalCost = new Money
-        {
-            Amount = 5000
-        };
-        var agentKey = new AgentKey
-        {
-            AgentId = "00801"
-        };
+            var totalCost = new Money
+            {
+                Amount = 5000
+            };
+            var agentKey = new AgentKey
+            {
+                AgentId = "00801"
+            };
 
-        var betLimitResult = betLimitsServiceClient.AddBetLimitReservation(customerId, totalCost, purchaseId).Result;
-        if (betLimitResult.Success)
-            Console.WriteLine($"customerId: {customerId} AddBetLimitReservation OK");
-        else
-            Console.WriteLine($"customerId: {customerId} AddBetLimitReservation failed: {betLimitResult.Message}, Color.Red");
-        
-        var registerTicketPurchaseResult = bettingServiceClient.RegisterTicketPurchaseDrafts(customerId, agentKey, purchaseId, betDataList, null).Result;
-        if (registerTicketPurchaseResult.Success)
-            Console.WriteLine($"customerId: {customerId} RegisterTicketPurchaseDrafts OK");
-        else
-            Console.WriteLine($"customerId: {customerId} RegisterTicketPurchaseDrafts failed: {betLimitResult.Message}", Color.Red);
+            var betLimitResult = betLimitsServiceClient.AddBetLimitReservation(customerId, totalCost, purchaseId).Result;
+            if (betLimitResult.Success)
+                Console.WriteLine($"customerId: {customerId} AddBetLimitReservation OK");
+            else
+                Console.WriteLine($"customerId: {customerId} AddBetLimitReservation failed: {betLimitResult.Message}", Color.Red);
 
-        var reserveResult = accountServiceClient.Reserve(customerId, totalCost, purchaseId, false).Result;
-        if (reserveResult.Success)
-            Console.WriteLine($"customerId: {customerId} Reserve OK");
-        else
-            Console.WriteLine($"customerId: {customerId} Reserve failed: {betLimitResult.Message}", Color.Red);
-        
-        var placeBetsResult = bettingServiceClient.PlaceBets(customerId, purchaseId).Result;
-        if (placeBetsResult.Success)
-            Console.WriteLine($"customerId: {customerId} PlaceBets OK");
-        else
-            Console.WriteLine($"customerId: {customerId} PlaceBets failed: {betLimitResult.Message}", Color.Red);
+            var registerTicketPurchaseResult = bettingServiceClient.RegisterTicketPurchaseDrafts(customerId, agentKey, purchaseId, betDataList, null).Result;
+            if (registerTicketPurchaseResult.Success)
+                Console.WriteLine($"customerId: {customerId} RegisterTicketPurchaseDrafts OK");
+            else
+                Console.WriteLine($"customerId: {customerId} RegisterTicketPurchaseDrafts failed: {registerTicketPurchaseResult.Message}", Color.Red);
+
+            var reserveResult = accountServiceClient.Reserve(customerId, totalCost, purchaseId, false).Result;
+            if (reserveResult.Success)
+            {
+                Console.WriteLine($"customerId: {customerId} Reserve OK");
+                WriteFile($"{customerId}={purchaseId.Id}{Environment.NewLine}", fileLocation);
+            }
+            else
+                Console.WriteLine($"customerId: {customerId} Reserve failed: {reserveResult.Message}", Color.Red);
+        }
+    }
+}
+
+void WriteFile(string fileContents, string filePathAndName)
+{
+    using (var mutex = new Mutex(false, filePathAndName.Replace("\\", "")))
+    {
+        var hasHandle = false;
+        try
+        {
+            hasHandle = mutex.WaitOne(Timeout.Infinite, false);
+            File.AppendAllText(filePathAndName, fileContents);
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        finally
+        {
+            if (hasHandle)
+                mutex.ReleaseMutex();
+        }
+    }
+}
+
+void DoBets(){
+    var customerDictionary = new Dictionary<string, string>();
+    void ParseToDictionary(string filePathName)
+    {
+        var lines = File.ReadLines(filePathName);
+        foreach (var line in lines)
+        {
+            var values = line.Split('=');
+            customerDictionary.Add(values[0], values[1]);
+        }
     }
 
-    Thread.Sleep(1000);
+    ParseToDictionary(fileLocation);
+
+    Thread.Sleep(3000);
+
+    for (int i = 0; i <= nrOfRunsBets - 1; i++)
+    {
+        var startingCustomerId = 8_000_001;
+        var nrOfTickets = 10;
+        var count = i * nrOfTickets;
+        var numbers = Enumerable.Range(startingCustomerId + count, nrOfTickets);
+
+        Parallel.ForEach(numbers,
+                        new ParallelOptions
+                        {
+                            MaxDegreeOfParallelism = Convert.ToInt32(Math.Ceiling(Environment.ProcessorCount * 0.75 * nrOfCores))
+                        },
+                        RunFromCmdPromptAsync);
+
+        void RunFromCmdPromptAsync(int customerId)
+        {
+            Console.WriteLine($"customerId: {customerId}");
+
+            var hostProvider = host.Services;
+            using IServiceScope serviceScope = hostProvider.CreateScope();
+            IServiceProvider provider = serviceScope.ServiceProvider;
+            var betLimitsServiceClient = provider.GetRequiredService<IBetLimitsService>();
+            var bettingServiceClient = provider.GetRequiredService<IBettingService>();
+            var accountServiceClient = provider.GetRequiredService<IAccountingService>();
+
+            var purchaseId = new PurchaseId
+            {
+                Id = customerDictionary[customerId.ToString()]
+            };
+
+            var placeBetsResult = bettingServiceClient.PlaceBets(customerId, purchaseId).Result;
+            if (placeBetsResult.Success)
+                Console.WriteLine($"customerId: {customerId} PlaceBets OK");
+            else
+                Console.WriteLine($"customerId: {customerId} PlaceBets failed: {placeBetsResult.Message}", Color.Red);
+        }
+    }
+    File.Delete(fileLocation);
 }
+
+//SetupReservations();
+//DoBets();
 
 await host.RunAsync();
